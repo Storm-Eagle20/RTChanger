85bf4a9bb3e9c1d7faba03cb0c98bf90451a7586
Storm-Eagle20 <johnfausnaught@gmail.com>
2017-07-13 09:32:40 -0400

    Update code.c

--- code.c
+++ code.c
@@ -1,15 +1,13 @@
-//Notifications to user after booting RTChanger.
-
 #include <stdio.h>
 #include <mcu.h>
 
 int main ()
 {
   gfxInit(GSP_RGB565_OES, GSP_BGR888_OES, false); //Inits both screens.
-  printf ( "Welcome to RTChanger! \n" );
+  printf ( "Welcome to RTChanger! \n" ); //Notifications to user after booting RTChanger.
   printf ( "Using this program, you can manually change the Raw RTC. \n" );
   printf ( "The Raw RTC is your hidden System Clock. Editing this allows you to bypass timegates. \n" );
-  printf ( "More information can be found at my GitHub. \n \n" ); //Filler text. No GitHub yet.
+  printf ( "More information can be found at my GitHub. \n \n" ); 
   printf ( "Press A to continue." );
     while ( aptmainloop() ) //Detects the input for the A button.
 {
@@ -18,7 +16,7 @@ int main ()
       if (kDown == KEY_A)
 {
         consoleClear();
-        printf ( "Please change the time below. It does not match your current time you see on the Home Menu, this is normal. Change the time by however much you need necessary, changing the time backwards is not recommended." );
+        printf ( "Please change the time below. It does not match your current time you see on the Home Menu, this is normal. Change the time by however much you need necessary, changing the time backwards is not recommended." ); //Text shown at the second screen.
 
           typedef struct
 {
