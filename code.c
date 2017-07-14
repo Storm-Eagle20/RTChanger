15b28de7c4f5ac6fe502dddfcdacc56c74775753
Storm-Eagle20 <johnfausnaught@gmail.com>
2017-07-14 15:33:00 -0400

    Update code.c

--- code.c
+++ code.c
@@ -25,22 +25,22 @@ int main ()
     printf ("Press A to continue or START to return to the Home Menu. \n \n \n \n");
     printf ("\x1b[36mhttps://www.github.com/Storm-Eagle20/RTChanger\x1b[0m");
     while (aptmainloop()) //Detects the input for the A button.
-        {    
+     {    
          hidscaninput();
          kDown = hidKeysDown(); //Detects if the A button was pressed.
          kHeld = hidKeysHeld(); //Detects if the A button was held.
          kUp = hidKeysUp();   //Detects if the A button was just released.
          if (kHeld & KEY_START) break; // User can choose to continue or return to the Home Menu.  
          if (kDown & KEY_A);
-            {
+         {
              consoleClear(); //Clears the screen of text.
              printf ("Please change the time below. It does not match your current time you see on the Home Menu, this is normal. /n"); //Text shown at the second screen.
              printf ("Change the time by however much you need necessary, \x1b[31mchanging the time backwards is not recommended.\x1b[0m"); 
-            } 
+         } 
          gfxFlushBuffers();
          gfxSwapBuffers();
          gspWaitForVBlank();
-        }
+     }
     gfxExit();
     return 0;
 }
