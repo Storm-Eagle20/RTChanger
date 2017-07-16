bf1fbb08ce3118bb637edaf90782768e7f547e9b
Storm-Eagle20 <johnfausnaught@gmail.com>
2017-07-15 20:11:32 -0400

    Update main.c

--- source/main.c
+++ source/main.c
@@ -10,7 +10,7 @@ typedef struct
     u8 something;
     u8 day;
     u8 month;
-    u8 year   
+    u8 year;   
 } RTC;
 
 void bcdfix(u8* wat)
@@ -21,7 +21,7 @@ void bcdfix(u8* wat)
 
 int main ()
 {
-    gfxInit(GSP_RGB565_OES, GSP_BGR888_OES, false); //Inits both screens.
+    gfxInit(GSP_RGB565_OES, GSP_BGR8_OES, false); //Inits both screens.
     PrintConsole topScreen, bottomScreen;
     consoleInit(GFX_TOP, &topScreen);
     consoleInit(GFX_BOTTOM, &bottomScreen);
@@ -35,9 +35,12 @@ int main ()
     printf ("\x1b[36mhttps://www.github.com/Storm-Eagle20/RTChanger\x1b[0m");
     consoleSelect(&topScreen);
     RTC rtctime;
+	u32 kDown = 0;
+	u32 kHeld = 0;
+	u32 kUp = 0;
     u8* buf = &rtctime;
     u8 offs = 0;
-    while (aptmainloop())                           //Detects the input for the A button.
+    while (aptMainLoop())                           //Detects the input for the A button.
     {    
          hidScanInput();
          kDown = hidKeysDown();        //Detects if the A button was pressed.
@@ -49,29 +52,29 @@ int main ()
          printf("Here you can change your time. Changing backwards is not recommended.");
          printf("Change your time by however you may need.");
         
-         if(kDown & (KEY_UP));         //Detects if the UP D-PAD button was pressed.
+         if(kDown & (KEY_UP))          //Detects if the UP D-PAD button was pressed.
          {    
              buf[offs]++ //Makes an offset increasing the original value by one.
              switch(offs)
-             {
-                case 0: //seconds
-                case 1: //minutes
-                    break;
+             {   
+                 case 0: //seconds
+                 case 1: //minutes
+                     break;
                     
-                case 2: //hours
-                    break;
+                 case 2: //hours
+                     break;
                     
-                case 4: //days
-                    break;
+                 case 4: //days
+                     break;
                      
-                case 5: //months
-                    break;
+                 case 5: //months
+                     break;
                     
-                case 6: //years
-                    break;
+                 case 6: //years
+                     break;
              }       
          }
-         if(kDown & (KEY_DOWN));       //Detects if the UP D-PAD button was pressed.
+         if(kDown & (KEY_DOWN))        //Detects if the UP D-PAD button was pressed.
          {    
              buf[offs]-- //Makes an offset decreasing the original value by one.
              switch(offs)
